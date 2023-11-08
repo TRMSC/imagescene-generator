fce482405702ece6042a4fc9c72875f3310cdcb6
TRMSC <solidware@web.de>
2023-11-08 19:14:19 +0100

    add time to filename

--- docs/src/script.js
+++ docs/src/script.js
@@ -518,12 +518,15 @@ downloadFile = (type) => {
   let html = document.getElementById('imagescene-result').value;
   let template = document.getElementById('imagescene-template').value.replace("raw", type);
 
-  // Get date for filename
+  // Get date and time for filename
   const date = new Date();
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
-  const currentDate = `${year}-${month}-${day}`;
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  const currentDate = `${year}-${month}-${day}-${hours}-${minutes}-${seconds}`;
 
   // Add XML declaration for SVG type
   if (type === 'svg') html = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n${html}`;
